df685d5d3a77512002d7bb68287e2a4a25e6794f
Valery Ost <v.k.ostapenko55@gmail.com>
2024-10-10 05:30:35 +0300

    Во вложенных циклах доступно внешнее окружение. Используются метки. Использование continue  по меткам

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
-
-
-        for (int i = 1; i <= 5; i++) {
-
-           
-            System.out.println("i = " + i);
+       M1:
+        for (int i = 3, j = 0; i > 0 ; i -- ) {
+            M2:
+            for ( ; j < 3 ; j++ ) {
+                System.out.println(i + "" + j);
+                if (j==0) continue M1;
+            }
         }
+
     }
 }
