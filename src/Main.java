b010f7c2e8230b0e33d2d2bc4aede99c30d6bdf1
Zl0yR0k <Zl0yR0k@mail.ru>
2024-10-18 19:46:55 +0300

    commit  repead

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
+<<<<<<< Updated upstream
        M1:
         for (int i = 3, j = 0; i > 0 ; i -- ) {
             M2:
@@ -9,6 +10,16 @@ public class Main {
                 System.out.println(i + "" + j);
                 if (j==0) continue M1;
             }
+=======
+        System.out.printf("Hello and welcome!");
+
+
+        for (int i = 1; i <= 5; i++) {
+
+
+            System.out.println("i = " + i);
+            System.out.println("WTF");
+>>>>>>> Stashed changes
         }
 
     }
