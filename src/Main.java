9b7eecc3a7ab2af53f38c14390ac29872ba7235c
fless <fless@inbox.ru>
2024-10-19 18:41:25 +0300

    fixed some problem with stash

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-<<<<<<< Updated upstream
+
        M1:
         for (int i = 3, j = 0; i > 0 ; i -- ) {
             M2:
@@ -10,16 +10,7 @@ public class Main {
                 System.out.println(i + "" + j);
                 if (j==0) continue M1;
             }
-=======
-        System.out.printf("Hello and welcome!");
 
-
-        for (int i = 1; i <= 5; i++) {
-
-
-            System.out.println("i = " + i);
-            System.out.println("WTF");
->>>>>>> Stashed changes
         }
 
     }
